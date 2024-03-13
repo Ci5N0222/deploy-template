eb8b2b07d2fdd21f52ef7414f5f89cdeaf0c0461
SionNoh <vmfpsel@gmail.com>
2024-03-13 14:04:37 +0900

    setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button1 from './components/post-css/Button1';
+import Button2 from './components/post-css/Button2';
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+    </>
+  );
+}
+
+export default App;
