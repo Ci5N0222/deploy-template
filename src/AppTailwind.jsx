bd7757426602f95e426d02096dfea13e18c20f12
SionNoh <vmfpsel@gmail.com>
2024-03-13 14:23:18 +0900

    setup

--- src/AppTailwind.jsx
+++ src/AppTailwind.jsx
@@ -21,7 +21,7 @@ export default function AppTailwind() {
   return (
     <>
       <h1 className='text-2xl'>안녕!</h1>
-      <button className='bg-blue-500 rounded-xl px-2'>Button</button>
+      <button className='bg-blue-500 rounded-xl px-2'>Button-Button</button>
     </>
   );
 }
