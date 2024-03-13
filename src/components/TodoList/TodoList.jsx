60e9823dfd66fad81e84fbdb1888370a02a1a749
SionNoh <vmfpsel@gmail.com>
2024-03-13 23:46:10 +0900

    setup

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,24 +1,27 @@
 import { useState } from "react"
 import AddTodo from "../AddToto/AddTodo"
+import Todo from "../../Todo/Todo";
 
 export default function TodoList() {
     const [todos, setTodos] = useState([
         {id:'1', text:'공부하기', status:'active'},
         {id:'2', text:'학원가기', status:'active'}
     ])
-    const handleAdd = (todo) => {
-        // 새로운 Todo를 todos에 업데이트
-        setTodos([...todos, todo]);
-    }
+    const handleAdd = (todo) => setTodos([...todos, todo])
+    const handleUpdate =(updated) => setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+    const handleDelete =(deleted) => setTodos(todos.filter((t)=>(t.id !== deleted.id)));
     return(
         <section>
             <ul>
                 {
-                    todos.map((item)=> {
-                        return(
-                            <li key={item.id}>{item.text}</li>
-                        )
-                    })
+                    todos.map((item)=>(
+                        <Todo 
+                            key={item.id} 
+                            todo={item} 
+                            onUpdate={handleUpdate} 
+                            onDelete={handleDelete}
+                        />
+                    ))
                 }
             </ul>
             <AddTodo onAdd={ handleAdd }/>
